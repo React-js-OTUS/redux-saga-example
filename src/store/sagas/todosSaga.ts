89159a7d88f8216f9fac8ec9144ea7632aee0fbc
Igor Zvyagin <spirit-drive@yandex.ru>
2023-11-20 23:08:05 +0300

    use calls

--- src/store/sagas/todosSaga.ts
+++ src/store/sagas/todosSaga.ts
@@ -1,9 +1,10 @@
-import { put, takeEvery } from 'redux-saga/effects';
+import { put, takeEvery, call } from 'redux-saga/effects';
 import { URL } from 'src/constants';
 
 export function* fetchTodosSaga() {
   try {
-    const todos = (yield fetch(`${URL}todos`).then((res) => res.json())) as unknown[];
+    const response: Response = yield call(fetch, `${URL}todos`);
+    const todos: unknown[] = yield call([response, 'json']);
     yield put({ type: 'todos/fetchTodosSuccess', payload: todos });
   } catch (error) {
     yield put({ type: 'todos/fetchTodosFailure', payload: error });
--- src/store/sagas/userSaga.ts
+++ src/store/sagas/userSaga.ts
@@ -1,12 +1,13 @@
-import { put, select, takeLatest } from 'redux-saga/effects';
+import { call, put, select, takeLatest } from 'redux-saga/effects';
 import { countActions, countSelectors } from 'src/store/count';
 import { URL } from 'src/constants';
-import { userActions } from 'src/store/user';
+import { User, userActions } from 'src/store/user';
 
 export function* fetchUser(): Generator {
   const count = (yield select(countSelectors.get)) as number;
-  const response = yield fetch(`${URL}users/${count}`).then((res) => res.json());
-  yield put(userActions.set(response));
+  const response = (yield call(fetch, `${URL}users/${count}`)) as Response;
+  const user = (yield call([response, 'json'])) as User;
+  yield put(userActions.set(user));
 }
 
 export function* userSaga() {
